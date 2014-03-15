8b5cffff922c1fc8d887f7bdb060de11e53e8e16
Brendon Verissimo <bverissimo@ucdavis.edu>
2014-03-15 10:23:23 -0700

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,103 @@
+// Ionic app App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'app' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'app.services' is found in services.js
+// 'app.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.services', 'app.controllers'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('side-bar', {
+      url: "/side",
+      abstract: true,
+      templateUrl: "templates/side-bar.html"
+    })
+
+    .state('side-bar.home', {
+      url: "/home",
+      views: {
+        'home-view': {
+          templateUrl: "templates/home.html",
+          controller: "HomeController"  
+        }
+      }
+    })
+
+    .state('side-bar.start', {
+      url: "/start",
+      views: {
+        'home-view': {
+          templateUrl: "templates/start.html",
+          controller: "StartController"
+        }
+      }
+    })
+
+    .state('side-bar.slides', {
+      url: '/start/:slideId',
+      views: {
+        'slide-views': {
+          templateUrl: 'templates/slide.html',
+          controller: 'SlideController'
+        }
+      }
+    });
+
+  // $stateProvider
+
+  //   // setup an abstract state for the tabs directive
+  //   .state('tab', {
+  //     url: "/tab",
+  //     abstract: true,
+  //     templateUrl: "templates/side-bar.html"
+  //   })
+
+  //   // the pet tab has its own child nav-view and history
+  //   .state('tab.pet-index', {
+  //     url: '/pets',
+  //     views: {
+  //       'pets-tab': {
+  //         templateUrl: 'templates/pet-index.html',
+  //         controller: 'PetIndexCtrl'
+  //       }
+  //     }
+  //   })
+
+  //   .state('tab.pet-detail', {
+  //     url: '/pet/:petId',
+  //     views: {
+  //       'pets-tab': {
+  //         templateUrl: 'templates/pet-detail.html',
+  //         controller: 'PetDetailCtrl'
+  //       }
+  //     }
+  //   })
+
+  //   .state('tab.adopt', {
+  //     url: '/adopt',
+  //     views: {
+  //       'adopt-tab': {
+  //         templateUrl: 'templates/adopt.html'
+  //       }
+  //     }
+  //   })
+
+  //   .state('tab.about', {
+  //     url: '/about',
+  //     views: {
+  //       'about-tab': {
+  //         templateUrl: 'templates/about.html'
+  //       }
+  //     }
+  //   });
+
+  // // if none of the above states are matched, use this as the fallback
+  // $urlRouterProvider.otherwise('/tab/pets');
+  $urlRouterProvider.otherwise('/side/home');
+
+});
+
---
+++ www/js/controllers.js
@@ -0,0 +1,12 @@
+angular.module('app.controllers', [])
+
+.controller('HomeController', ['$scope', function($scope) {
+}])
+
+.controller('StartController', ['$scope', function($scope) {
+  
+}])
+
+.controller('SlideController', ['$scope', function($scope) {
+
+}]);
