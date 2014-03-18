2aaf8e8d861696a7662fa47bf79adecf79d426dd
Adam D Richman <ar@adamrichman.com>
2014-03-17 17:26:28 -0700

    reduce database storage size

--- www/js/services.js
+++ www/js/services.js
@@ -42,16 +42,16 @@ angular.module('app.services', [])
   this.createDB = function() {
     var d = $q.defer();
     Cordova.navigator().then(function(navigator) {
-      var db = window.openDatabase("test", "1.0", "Test_DB", 1000000000); 
+      var db = window.openDatabase("test", "1.0", "Test_DB", 10000); 
       db.transaction(function(tx) {
         //tx.executeSql('PRAGMA foreign_keys = ON');
         //tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id INTEGER, name VARCHAR, stitchFix VARCHAR, last_played INTEGER)');
         // tx.executeSql('CREATE TABLE IF NOT EXISTS PHOTOS (id INTEGER PRIMARY KEY, name VARCHAR, rank INTEGER,
         //                FOREIGN KEY (category) REFERENCES CATEGORIES (id))');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS CATEGORIES (id INTEGER, name VARCHAR, score INTEGER)');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY, name VARCHAR, stitchFix VARCHAR, last_played DATE)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS CATEGORIES (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, score INTEGER)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, stitchFix VARCHAR, last_played DATE)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS ADAM (name VARCHAR)');
       });
-      d.resolve(db);
     });
     return d.promise;
   };
@@ -59,16 +59,16 @@ angular.module('app.services', [])
   this.updateDB = function(info) {
     var last_played = new Date();
     console.log(last_played, info);
-    var db = window.openDatabase("test", "1.0", "Test_DB", 1000000000);
+    var db = window.openDatabase("test", "1.0", "Test_DB", 10000);
     db.transaction(function(tx) {
       //tx.executeSql('PRAGMA foreign_keys = ON');
-      tx.executeSql('INSERT INTO USER (id) VALUES (?)',
-        [7],
+      tx.executeSql('INSERT INTO ADAM (name) VALUES (?)',
+        ["Adam"],
       function(tx, result) {
-        console.log("success!", result);
+        console.log("update success!", result);
       },
       function(tx, result) {
-        console.log("error!", result);
+        console.log("update error!", result);
       });
       
     }); 
