19dbd8ff87ac69b5f37a48093b360594d79ebdb7
Adam D Richman <ar@adamrichman.com>
2014-03-18 15:06:40 -0700

    start method to calculate scores in Game Service

--- www/js/GameService.js
+++ www/js/GameService.js
@@ -1,7 +1,13 @@
 angular.module('app.services')
 
 .service('EndOfGameService', ['$state', function($state){
+  
+  var calculateScores = function(prefs){
+    return {};
+  };
+
+
   return  function(prefs){
-            $state.go('results', { preference: prefs });
+            $state.go('results', { preference: calculateScores(prefs) });
           };
 }]);
