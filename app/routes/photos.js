91da3fe9d53e838ce6e17d2924d76d2defd37129
Adam D Richman <ar@adamrichman.com>
2014-03-18 23:03:22 -0700

    server returns proper json

--- app/routes/photos.js
+++ app/routes/photos.js
@@ -3,7 +3,18 @@ var photos = require('../controllers/photos');
 
 module.exports = function(app) {
 
-  app.get('/images', photos.getAll);
+  app.get('/images', function(request, response) {
+    console.log('/images', __dirname);
+    fs.readFile(__dirname + '/../../app/data/data_img_small.json', 'utf-8', function(err, data) {
+      if( err ) {
+        response.send(500, "ERROR", err);
+      } else {
+        data = JSON.stringify(data);
+        console.log(data);
+        response.send(200, data);
+      }
+    });
+  });
 
   app.post('/images', photos.create);
 
