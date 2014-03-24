f293b1a8c6a4730adecb1852eb74b37fa7b22a7c
Adam D Richman <ar@adamrichman.com>
2014-03-24 00:37:01 -0700

    improves UI and allows cards to snap back without commiting

--- www/js/HomeController.js
+++ www/js/HomeController.js
@@ -2,27 +2,19 @@ angular.module('app.controllers', ['ionic.contrib.ui.cards', 'app.services'])
 
 
 .controller('HomeController', ['$rootScope','$scope', '$state', '$timeout', '$ionicLoading','ENV', function($rootScope, $scope, $state, $timeout, $ionicLoading, ENV)  {
-  $scope.showSpinner = false;
-  $scope.loading = $ionicLoading.show(ENV.loadingOptions);
+  $scope.showLoading = false;
 
   $scope.$on('$viewContentLoading', function() {
   });
   
   $scope.$on('$viewContentLoaded', function() {
-    $timeout(function(){
-      $scope.loading.hide();
-    },3000)
   });
 
   $rootScope.$on('$stateChangeStart', function() {
-    $scope.loading = $ionicLoading.show(ENV.loadingOptions);
   });
 
   $rootScope.$on('$stateChangeSuccess', function() {
     $rootScope.level = $state.params.level ? $state.params.level : 1;
-    $timeout(function(){
-      $scope.loading.hide();
-    },1000)
   });
 
   $scope.go = function(state) {
