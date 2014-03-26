898251b647d3f162d05e36319a8530e102538115
Brendon Verissimo <bverissimo@ucdavis.edu>
2014-03-26 09:48:04 -0700

    style additions

--- www/js/ResultsController.js
+++ www/js/ResultsController.js
@@ -2,6 +2,7 @@ angular.module('app.controllers')
 
 .controller('ResultsController', ['$scope', '$state', '$stateParams', '$http', '$location', 'UserService','$ionicLoading', 'ENV', function($scope, $state, $stateParams, $http, $location, UserService, $ionicLoading, ENV) {
   $scope.args = $stateParams;
+  console.log($scope.args);
   // $scope.list = [{'id':1,'title':'hey'},{'id':2,'title':'sup'},{'id':3,'title':'nothin'}];
 
   $scope.postFB = function() {
@@ -9,7 +10,7 @@ angular.module('app.controllers')
     UserService.currentUser().then(function(user) {
         console.log(user);
         //$http.post('https://graph.facebook.com/' + user.id + '/feed?message=&access_token=' + user.accessToken )
-        $http.post('https://graph.facebook.com/' + user.id + '/feed?link=http://www.stitchfix.com&picture=https://d27bvhtwhzf6pr.cloudfront.net/assets/facebook-share-banner.jpg&message=Look\40at\40my\40style!&access_token=' + user.accessToken )
+        $http.post('https://graph.facebook.com/' + user.id + '/feed?link=http://www.stitchfix.com&picture=https://d27bvhtwhzf6pr.cloudfront.net/assets/facebook-share-banner.jpg&message=My\40style\40is:\40' + $scope.args.preference + '!&access_token=' + user.accessToken )
         .success(function(data, status, headers, config) {
           console.log(arguments);
         })
